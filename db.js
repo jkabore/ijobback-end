4df977f3477a89a53612cafecee7488faf777750
jkabore1 <jkabore1@gmail.com>
2023-07-18 05:09:22 -0400

    first commit

--- db.js
+++ db.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
-dbConnect()
-async function dbConnect(){
-
-     try {
-         await mongoose.connect('mongodb+srv://jkabore1:comptedeDieu1@cluster0.cbbgtiu.mongodb.net/jobs?retryWrites=true&w=majority' , {useNewUrlParser : true});
-         console.log('Mongo DB Connection success')
-     } catch (error) {
-         console.log('Mongo DB Connection failed')
-     }
-
+let pass = process.env.MongoDB;
+dbConnect();
+async function dbConnect() {
+  try {
+    await mongoose.connect(pass, { useNewUrlParser: true });
+    console.log("Mongo DB Connection success");
+  } catch (error) {
+    console.log("Mongo DB Connection failed");
+  }
 }
 
 module.exports = mongoose;
