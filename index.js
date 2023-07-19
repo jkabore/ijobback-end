ad9692fcf260801ef437ec03df0ceaa2012ad4dd
jkabore1 <jkabore1@gmail.com>
2023-07-19 18:17:03 -0400

    first commit

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(
   cors({
     origin: "https://ijobclient.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 app.use("/api/jobs", jobsRoute);
